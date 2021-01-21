af71928de0ea049ab993d0d977b95d88fb2c9727
Will Snyder <willsnyder99@gmail.com>
2021-01-20 22:58:18 -0800

    Add files via upload

---
+++ interface/api/index.js
@@ -0,0 +1,151 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const cors = require("cors");
+const node_json_db_1 = require("node-json-db");
+const JsonDBConfig_1 = require("node-json-db/dist/lib/JsonDBConfig");
+const app = express();
+app.use(cors());
+// Initialize Express as simple HTTP server
+const server = http.createServer(app);
+// Initialize WebSocket instance bootstrapping from Express
+const wss = new WebSocket.Server({ server });
+// Initialize the reactor database
+let reactorDB = new node_json_db_1.JsonDB(new JsonDBConfig_1.Config("reactorDataBase", true, true, '/'));
+// WebSocket event handlers
+wss.on('connection', (ws) => {
+    console.log('New client has connected!');
+    ws.send('Hello!');
+    ws.on('message', function incoming(message) {
+        parseMessage(message, ws);
+    });
+    ws.on('close', function close() {
+        console.log("A client has disconnected!");
+    });
+});
+function parseMessage(message, ws) {
+    let obj = JSON.parse(message);
+    // Get next available ID
+    if (obj.task == "get-id") {
+        console.log("Client has requested a new ID");
+        ws.send(getNextID());
+    }
+    // Create new reactor
+    if (obj.task == "new") {
+        console.log("Creating new reactor");
+        newReactor(obj.ID, obj.NAME, obj.WORLD);
+    }
+    // Update reactor status
+    if (obj.task == "update-status") {
+        console.log(`Updating reactor status for ID ${obj.ID}`);
+        updateReactor(obj);
+    }
+    // Update reactor error
+    if (obj.task == "update-error") {
+        console.log(`Updating PID error for ID ${obj.ID}`);
+        updatePIDError(obj);
+    }
+    // Execute PID and send back rod adjustment to client
+    if (obj.task == "run-pid") {
+        console.log(`Calculating PID adjust for ID ${obj.ID}`);
+        ws.send(runPID(obj.ID));
+    }
+}
+function runPID(ID) {
+    const Kp = reactorDB.getData(`/${ID}/pidOptions/Kp`);
+    const Ki = reactorDB.getData(`/${ID}/pidOptions/Ki`);
+    const Kd = reactorDB.getData(`/${ID}/pidOptions/Kd`);
+    const error = reactorDB.getData(`/${ID}/reactorError/error`);
+    return Math.round(Kp * error + Ki * integratedError(error, ID) + Kd * derivedError(error, ID));
+}
+function integratedError(error, ID) {
+    let intErrTotal;
+    const integralMin = -50;
+    const integralMax = 50;
+    try {
+        intErrTotal = reactorDB.getData(`/${ID}/pidStatus/integralError`);
+    }
+    catch (error) {
+        console.log('No integral error found! Getting default');
+        intErrTotal = 1;
+    }
+    intErrTotal = intErrTotal + error;
+    if (intErrTotal > integralMax) {
+        intErrTotal = integralMax;
+    }
+    if (intErrTotal < integralMin) {
+        intErrTotal = integralMin;
+    }
+    reactorDB.push(`/${ID}/pidStatus/integralError`, intErrTotal);
+    return intErrTotal;
+}
+function derivedError(error, ID) {
+    let energyHistory;
+    let energyProduction = 0;
+    try {
+        energyHistory = reactorDB.getData(`/${ID}/pidStatus/lastEnergyPoll`);
+        energyProduction = reactorDB.getData(`/${ID}/reactorStatus/rfProduced`);
+    }
+    catch (error) {
+        console.log('No energy history found! Setting default');
+        energyHistory = 1;
+    }
+    reactorDB.push(`/${ID}/pidStatus/lastEnergyPoll`, energyProduction);
+    return energyProduction - energyHistory;
+}
+function updateReactor(reactorData) {
+    reactorDB.push(`/${reactorData.ID}/reactorStatus`, {
+        isActive: reactorData.activity,
+        energyStored: reactorData.energy,
+        energySaturation: reactorData.saturation,
+        fuelLevel: reactorData.fuel,
+        wasteLevel: reactorData.waste,
+        fuelMax: reactorData.maxFuel,
+        wasteProduced: reactorData.fuelBurnup,
+        rfProduced: reactorData.energyOut,
+        reactivity: reactorData.coreReactivity,
+        temp: reactorData.coreTemp
+    });
+}
+function updatePIDError(reactorData) {
+    reactorDB.push(`/${reactorData.ID}/reactorError`, {
+        error: reactorData.rfUsage
+    });
+}
+function newReactor(ID, NAME, WORLD) {
+    // Create a new reactor and set default PID gain tunings (Kp = 1, Ki = 1, Kd = 1)
+    reactorDB.push(`/${ID}`, { name: NAME, world: WORLD, pidOptions: { Kp: 1, Ki: 1, Kd: 1, time: 0.5 } });
+}
+function getNextID() {
+    let running = true;
+    let i = 0;
+    while (running) {
+        try {
+            reactorDB.getData(`/${i}`);
+            ++i;
+        }
+        catch (error) {
+            running = false;
+        }
+    }
+    return i;
+}
+app.get('/', function (req, res) {
+    console.log('Sending Data to React Front-end');
+    const len = getNextID();
+    let data = [];
+    for (let i = 0; i < len; ++i) {
+        data.push(reactorDB.getData(`/${i}/`));
+    }
+    res.send(data);
+});
+// Start the server
+app.listen(9000, () => {
+    console.log(`Starting Express Server on port 9000`);
+});
+server.listen(8080, () => {
+    console.log(`Starting WebSocket Server on port 8080`);
+});
+//# sourceMappingURL=index.js.map
